c9904a29219c093ee26b0763bbd97ad1bb010b7c
huangqiaowei <huangqiaowei@huatu.com>
2019-07-24 17:11:18 +0800

    刷题

--- src/main/java/study/leetcode/month2/day19/Solution.java
+++ src/main/java/study/leetcode/month2/day19/Solution.java
@@ -1,38 +1,34 @@
-package study.leetcode.month2.day18;
+package study.leetcode.month2.day19;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author huangqiaowei
- * @date 2019-07-22 21:38
+ * @date 2019-07-23 22:57
  **/
 public class Solution {
 
     /**
-     * 给定一个没有重复数字的序列，返回其所有可能的全排列。
-     * <p>
+     * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+     *
      * 示例:
-     * <p>
-     * 输入: [1,2,3]
+     *
+     * 输入: [1,1,2]
      * 输出:
      * [
-     * [1,2,3],
-     * [1,3,2],
-     * [2,1,3],
-     * [2,3,1],
-     * [3,1,2],
-     * [3,2,1]
+     *   [1,1,2],
+     *   [1,2,1],
+     *   [2,1,1]
      * ]
-     * <p>
-     * 来源：力扣（LeetCode）
-     * 链接：https://leetcode-cn.com/problems/permutations
-     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/permutations-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      * @param nums 给定的数组
-     * @return 全排列结果
+     * @return 结果
      */
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
 
         if (nums == null || nums.length == 0) {
             return null;
@@ -53,14 +49,36 @@ public class Solution {
             list.add(real);
             return;
         }
-        for (Integer i : temp) {
-            result.add(nums[i]);
+        for (int i = 0; i < temp.size(); i++) {
+            if (isFilter(i,temp,nums)){
+                continue;
+            }
+            result.add(nums[temp.get(i)]);
             List<Integer> objects = new ArrayList<>();
             objects.addAll(temp);
-            objects.remove(i);
+            objects.remove(temp.get(i));
             backtracking(list, objects, result, nums);
-            result.remove(Integer.valueOf(nums[i]));
+            result.remove(result.size() - 1);
         }
     }
+
+    public boolean isFilter(int i, List<Integer> temp,int[] nums){
+        if (i == 0) {
+            return false;
+        }
+        for (int j = 0; j < i; j++) {
+            if (nums[temp.get(i)] == nums[temp.get(j)]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.permuteUnique(new int[]{1,1,2,2});
+    }
+
 
 }
